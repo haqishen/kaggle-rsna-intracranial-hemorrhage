8883ac1ba0f1dd63da2412f0759d94c5c12a32b1
qishen-ha <qishen.ha@linecorp.com>
2019-10-29 13:53:21 +0900

    hotpush

--- bin/predict_effnet_b3_2.sh
+++ bin/predict_effnet_b3_2.sh
@@ -1,5 +1,5 @@
-model=model001
-folder=seres50_2
+model=effnet_b3_2
+folder=effnet_b3_2
 gpu=0
 fold=0
 ep=2
@@ -14,5 +14,5 @@ sub=/data/src/kaggle-rsna-intracranial-hemorrhage/exp/${folder}/${model}_fold${f
 
 python -m src.cnn.main test ${conf} --snapshot ${snapshot} --output ${test} --n-tta ${tta} --fold ${fold} --gpu ${gpu}
 python -m src.postprocess.make_submission --input ${test} --output ${sub} --clip ${clip}
-#kaggle competitions submit rsna-intracranial-hemorrhage-detection -m "" -f ./data/submission/${sub}
+kaggle competitions submit rsna-intracranial-hemorrhage-detection -m "dalao's code /w effnet_b3" -f ${sub}
 
