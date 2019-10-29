14f78150a2670c90a31e2b5086e0ccd290ac1387
qishen-ha <qishen.ha@linecorp.com>
2019-10-29 13:43:22 +0900

    hotpush

--- bin/predict001.sh
+++ bin/predict001.sh
@@ -12,7 +12,7 @@ valid=/data/src/kaggle-rsna-intracranial-hemorrhage/exp/${folder}/fold${fold}_ep
 test=/data/src/kaggle-rsna-intracranial-hemorrhage/exp/${folder}/fold${fold}_ep${ep}_test_tta${tta}.pkl
 sub=/data/src/kaggle-rsna-intracranial-hemorrhage/exp/${folder}/${model}_fold${fold}_ep${ep}_test_tta${tta}.csv
 
-python -m src.cnn.main test ${conf} --snapshot ${snapshot} --output ${test} --n-tta ${tta} --fold ${fold} --gpu ${gpu}
+# python -m src.cnn.main test ${conf} --snapshot ${snapshot} --output ${test} --n-tta ${tta} --fold ${fold} --gpu ${gpu}
 python -m src.postprocess.make_submission --input ${test} --output ${sub} --clip ${clip}
 #kaggle competitions submit rsna-intracranial-hemorrhage-detection -m "" -f ./data/submission/${sub}
 
