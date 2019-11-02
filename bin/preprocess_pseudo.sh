9e4a2626cc52306a18c7e35050fd435a199a7aa6
qishen-ha <qishen.ha@linecorp.com>
2019-11-02 23:30:21 +0900

    1

--- bin/preprocess_pseudo.sh
+++ bin/preprocess_pseudo.sh
@@ -1,7 +1,5 @@
-mkdir -p cache model data/submission
-
 # pseudo
-python -m src.postprocess.make_pseudo --input /data/src/kaggle-rsna-intracranial-hemorrhage/exp/effnet_b3_512_p2/effnet_b3_512_p2_5fold_5tta.csv --output /data/src/kaggle-rsna-intracranial-hemorrhage/exp/effnet_b3_512_p2/pseudo.csv
+python -m src.postprocess.make_pseudo --input /data/data/RSNA/new30fold.csv --output /data/src/kaggle-rsna-intracranial-hemorrhage/exp/effnet_b3_512_p2/pseudo.csv
 python -m src.preprocess.dicom_to_dataframe --input /data/src/kaggle-rsna-intracranial-hemorrhage/exp/effnet_b3_512_p2/pseudo.csv --output /data/data/RSNA/pseudo_v1_raw.pkl --imgdir /data/data/RSNA/stage_1_test_images
 python -m src.preprocess.create_dataset --input /data/data/RSNA/pseudo_v1_raw.pkl --output /data/data/RSNA/pseudo_v1.pkl
 
